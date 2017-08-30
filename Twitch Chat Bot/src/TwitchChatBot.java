d595fe459ca601212413c0e03d633ead096851eb
InfernOwl <cchampagnie.1991@gmail.com>
2017-08-29 22:36:50 -0400

    Commiting changes to the twitch chat bot. Added an onJoin command to get the current mod list when the bot starts up Added incomplete functions to add and remove mods

--- Twitch Chat Bot/src/TwitchChatBot.java
+++ Twitch Chat Bot/src/TwitchChatBot.java
@@ -1,9 +1,22 @@
 
 public class TwitchChatBot {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		
+		// Start the bot up
+		TwitchBot bot = new TwitchBot();
+		
+		// Enable debugging
+		bot.setVerbose(true);
+		
+		// Connect to the IRC server
+		bot.connect("irc.chat.twitch.tv", 6667, "oauth:gshy0f2egx4sa1szd2iced4wohnsbc");
+		
+		// Join the channel
+		bot.joinChannel("#wasterbot");
+		
+		
 	}
 
 }
